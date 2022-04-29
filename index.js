98100f5df73531973beef55fbabae20fc1c7ebf9
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-04-29 12:28:39 -0300

    Adiciona requisito 1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_req, res) => {
+  const talkers = fs.readFileSync('./talker.json');
+
+  return res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
