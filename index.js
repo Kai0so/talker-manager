48570879edaffd8fd48c3cddf4c5cec8b3fb9da3
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-04-29 13:16:53 -0300

    Adiciona requisito 2

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
 const PORT = '3000';
 
-app.get('/talker', (_req, res) => {
-  const talkers = fs.readFileSync('./talker.json');
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile('./talker.json');
 
   return res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('./talker.json');
+  const talker = JSON.parse(talkers).find((t) => t.id === parseFloat(id));
+
+  if (!talker) {
+    return res.status(HTTP_NOTFOUND_STATUS).json(
+      {
+        message: 'Pessoa palestrante não encontrada',
+      },
+    );
+  }
+
+  return res.status(HTTP_OK_STATUS).json(talker);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
